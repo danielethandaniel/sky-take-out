3f0a099c4e3da11656fdc4f617dec58bfef8681b
danielethandaniel <daniel2382335496@gmail.com>
2026-08-29 20:59:46 +0800

    启用禁用员工账号代码开发

--- sky-server/src/main/java/com/sky/controller/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/EmployeeController.java
@@ -1,10 +1,12 @@
-package com.sky.controller.admin;
+package com.sky.controller;
 
 import com.sky.constant.JwtClaimsConstant;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.EmployeeService;
 import com.sky.utils.JwtUtil;
@@ -13,10 +15,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -89,6 +88,34 @@ public class EmployeeController {
         log.info("新增员工：{}",employeeDTO);
         System.out.println("当前线程id" + Thread.currentThread().getId());
         employeeService.save(employeeDTO);
+        return Result.success();
+    }
+
+    /**
+     * 员工分页查询
+     * @param employeePageQueryDTO
+     * @return
+     */
+    @GetMapping("/page")
+    @ApiOperation("员工分页查询")
+    public Result<PageResult> page(EmployeePageQueryDTO employeePageQueryDTO){
+        log.info("员工分页查询：{}",employeePageQueryDTO);
+//        返回PageResult
+        PageResult pageResult =  employeeService.pageQuery(employeePageQueryDTO);
+        return Result.success(pageResult);
+    }
+
+    /**
+     * 启用/禁用员工账号
+     * @param status
+     * @param id
+     * @return
+     */
+    @PostMapping("/status/{status}")
+    @ApiOperation("启用/禁用员工账号")
+    public Result startOrstop(@PathVariable Integer status,Long id){
+        log.info("启用员工账号：{},{}",status,id);
+        employeeService.startOrstop(status,id);
         return Result.success();
     }
 }
